588bcdd99f4ea8e8edf9e80acbaeba7f1c8a9d64
Orange <dunduk201@yandex.ru>
2022-10-16 20:33:44 +0900

    Added condition if

---
+++ Program_005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "азамат")
+{
+    Console.WriteLine("Ура, Это же АЗАМАТ!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
